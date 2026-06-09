f1ce2e959e98b30f2046e02c6e23aea2aa125630
sahildevrajsocial <sdevrajsocial@gmail.com>
2026-06-09 16:27:16 +0530

    learned if, else if, else and also learned how to use prompt in javascript

---
+++ javascript/conditional statement using prompt in javascript/script.js
@@ -0,0 +1,23 @@
+const dayOfWeek = prompt('Enter a day');
+
+if (dayOfWeek === 'Monday') {
+    console.log("1st day");
+}
+else if (dayOfWeek === 'Tuesday') {
+    console.log("2th day");
+}
+else if (dayOfWeek === 'Wednesday') {
+    console.log("3th day");
+}
+else if (dayOfWeek === 'Thursday') {
+    console.log("4th day");
+}
+else if (dayOfWeek === 'Friday') {
+    console.log("5th day");
+}
+else if (dayOfWeek === 'Saturday') {
+    console.log("6th day");
+}
+else {
+    console.log("7th day")
+}
