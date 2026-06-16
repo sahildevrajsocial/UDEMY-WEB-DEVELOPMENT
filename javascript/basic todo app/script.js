c878a6a677d81921bcae0743e818c9b00aa1224f
sahildevrajsocial <sdevrajsocial@gmail.com>
2026-06-16 16:50:52 +0530

    made a basic todo app

---
+++ javascript/basic todo app/script.js
@@ -0,0 +1,28 @@
+let input = prompt("what would you like to do?")
+const todos = ['Collect Chicken Eggs', 'Clean Cat Litter Box']
+while (input !== 'quit' & input !== 'q') {
+    if (input === 'list') {
+        console.log('**************');
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log('**************');
+    }
+    else if (input === 'new') {
+        const newTodo = prompt('Ok, what is the new todo');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list`);
+    }
+    else if (input === 'delete') {
+        const index = parseInt(prompt('Ok, what todo do you want to delete?'));
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`ok, deleted ${deleted[0]}`);
+        }
+        else {
+            console.log('unknown index!')
+        }
+    }
+    input = prompt("what would you like to do?")
+}
+console.log("OK QUIT THE APP!")
